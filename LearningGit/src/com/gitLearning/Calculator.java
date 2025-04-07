c78a737fe54e5aaea878833b49961cd7cf4a5321
zuleima <zuleimacruz91@yahoo.com>
2025-04-07 16:39:59 -0400

    Edited add method and added multiply method.

--- LearningGit/src/com/gitLearning/Calculator.java
+++ LearningGit/src/com/gitLearning/Calculator.java
@@ -3,7 +3,13 @@ package com.gitLearning;
 public class Calculator {
 	
 	public int add(int x, int y) {
-		return x + y;
+		int result = 0;
+		result = x + y;
+		return result;
+	}
+	
+	public int multiply(int x, int y) {
+		return x * y;
 		
 		
 	}
